5c821e9a1a7736d7044d691be5ecd0f26c0f9abe
Shehan Maxwell <shehanbbn@gmail.com>
2023-02-27 20:02:31 +0530

    completed string functions

--- intro/7.stringFunctions.cpp
+++ intro/7.stringFunctions.cpp
@@ -45,23 +45,63 @@ int main()
     
     // cout << "Your email is now: " << email;
 
-    string first_name;
-    string middle_name;
-    string last_name;
+    // string first_name;
+    // string middle_name;
+    // string last_name;
 
-    cout << "Enter your first name: ";
-    getline(cin, first_name);
-    cout << "Enter your middle name: ";
-    getline(cin, middle_name);
-    cout << "Enter your last name: ";
-    getline(cin, last_name);
+    // cout << "Enter your first name: ";
+    // getline(cin, first_name);
+    // cout << "Enter your middle name: ";
+    // getline(cin, middle_name);
+    // cout << "Enter your last name: ";
+    // getline(cin, last_name);
 
-    char letter1 = first_name.at(0);
-    char letter2 = middle_name.at(0);
-    char letter3 = last_name.at(0);
+    // char letter1 = first_name.at(0);
+    // char letter2 = middle_name.at(0);
+    // char letter3 = last_name.at(0);
 
-    cout << "Your initialise are: " << letter1 << letter2 << letter3;
+    // cout << "Your initialise are: " << letter1 << letter2 << letter3;
+    // string first_name;
+    
+    // cout << "Enter your first name: ";
+    // getline(cin, first_name);
 
+    // string nickname = first_name.substr(0,3); 
+
+    // cout << "Your nickname is: " << nickname << endl;
+    // cout << "Hello" << nickname;
+
+    // string1.insert(int x,string2)
+    // string user_name;
+    // cout << "Enter your user name: ";
+    // getline(cin, user_name);
+    
+    // user_name.insert(0, "@");
+
+    // cout << "reply: " << user_name;
+    // string1.find(string2); -returns index of string2
+
+    // string first_name;
+    // cout << "Enter your first name: ";
+    // getline(cin, first_name);
+    
+    // string substring;
+    // cout << "Search for which character/s?: ";
+    // getline(cin, substring);
+
+    // int position = first_name.find(substring);
+    
+    // cout << "Found character/s at position: " << position;
+
+    // string1.erase(x,y) erase portion of a string.
+
+    string phone_number;
+    cout << "Enter your phone number: ";
+    getline(cin, phone_number);
+
+    phone_number.erase(0, 4);
+
+    cout << "phone number: " << phone_number;
     cout << endl;
     return 0;
 }
