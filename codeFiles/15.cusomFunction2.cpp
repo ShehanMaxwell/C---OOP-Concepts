b452722513dc5d2a41d13a17b6d1619d833d0524
Shehan Maxwell <shehanbbn@gmail.com>
2023-02-28 14:42:56 +0530

    added custom function 2

--- codeFiles/15.cusomFunction2.cpp
+++ codeFiles/15.cusomFunction2.cpp
@@ -3,12 +3,31 @@
 #include <algorithm>
 using namespace std;
 
+double add(double num1, double num2) {
 
+    double result = num1 + num2;
+    return result; 
+
+
+};
 
 
 
 int main()
 {
+
+    double number1;
+    double number2;
+
+    cout << "Enter number 1: " << endl;
+    cin >> number1;
+
+    cout << "Enter number 2: " << endl;
+    cin >> number2;
+
+    double result = add(number1, number2);
+
+    cout << "Your result is: " << result << endl;
     
 
     cout << endl;
