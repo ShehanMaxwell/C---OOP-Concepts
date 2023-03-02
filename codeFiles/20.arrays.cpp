f67fb96c9884c3bd2a5cf7dda1c861ec2d1123a9
Shehan Maxwell <shehanbbn@gmail.com>
2023-03-02 22:45:09 +0530

    added pointers and arrays

---
+++ codeFiles/20.arrays.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string cars [] = { "Corvette" , "Tesla" , "Ferrari" };
+
+    cout << cars[0] << endl;
+
+    cout << endl;
+    return 0;
+}
